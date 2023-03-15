a19f2cf69a426db29a7a68c258e2bf13bac5fd6f
enisokic <enis-okic@hotmail>
2023-03-15 12:36:03 +0100

    mongoDB status veranderen werkt niet

--- server.js
+++ server.js
@@ -7,69 +7,89 @@ app
 	.set("view enige", "ejs")
 	.set("views", "./views");
 
-
-
-require("dotenv").config()
+require("dotenv").config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 console.log(uri);
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
 // const client = new MongoClient(uri);
 
-client.connect(err => {
-	console.log('it connects')
-	if (err) { throw eer }
+client.connect((err) => {
+	console.log("it connects");
+	if (err) {
+		throw err;
+	}
 });
+
+
+let db
+let kattenData
+let userData
 
 async function run() {
-    try {
-        await client.connect();
-        // database and collection code goes here
-        const db = client.db("catmatch-data");
-        const kattenData = db.collection("katten");
-        const userData = db.collection("user");
-        // find code goes here
-        const katten = kattenData.find();
-        const user = userData.find();
-        console.log(katten)
-    } catch(error) {
-        console.log(error)
-    }
-}
-run()
+	try {
+	  await client.connect();
+	  console.log("hi");
+	  // database and collection code goes here    
+	  db = client.db("catmatch-data");
+	  kattenData = db.collection("katten");
+	  userData = db.collection("user");
 
 
-
-
-const katten = require("./data/katten.json"); // Json bestand met data geimporteerd die nu de naam katten heeft zodat ik deze data kan gebruiken in mijn code
-const user = require("./data/user");
-
-app.get("/", (req, res) => {
-	// Home pagina routing
-	const eersteKat = katten.find((kat) => kat.status === "new"); // Zoekt de eerst volgende kat met status "new"
-
-	res.render("verkennen.ejs", { eersteKat }); // Toont de pagina verkennen met de juiste data
-
-	// console.log(katten); // Alle katten data om te controleren of de status is veranderd naar liked of disliked
-});
-
-app.get("/match", (req, res) => {
-	res.render("match.ejs", { katten }); // Match pagina met als route /match
-});
-
-app.post("/liked", (req, res) => {
-	const eersteKat = katten.find((kat) => kat.status === "new"); // katten.find etc. in elke app.post/.get waar nodig is zodat eersteKat elke keer wordt uitgevoerd.
-
-	if (user.likedBy.includes(eersteKat.id)) {
-		eersteKat.status = "liked"; // De kat status veranderd naar "liked"
-		res.redirect("/match"); // Alleen als de kat de user ook heeft geliked wordt de user doorgestuurd naar /match (match pagina)
-	} else {
-		eersteKat.status = "liked";
-		res.redirect("/"); // De user wordt doorgestuurd naar home (refresh want de user is al op home)
+	} catch (error) {
+	  console.log(error);
 	}
+  }
+  run();
+
+
+// const katten = require("./data/katten.json"); // Json bestand met data geimporteerd die nu de naam katten heeft zodat ik deze data kan gebruiken in mijn code
+// const user = require("./data/user");
+
+ app.get("/", async (req, res) => {
+	// Home pagina routing
+	try {
+		const eersteKat = await kattenData.findOne({ status: "new" }); // Zoekt de eerst volgende kat met status "new"
+		
+		res.render("verkennen.ejs", { eersteKat }); // Toont de pagina verkennen met de juist data
+
+		console.log(katten);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+app.get("/match", async (req, res) => {
+	try {
+		res.render("match.ejs", { katten }); // Match pagina met als route /match
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+app.post("/liked", async (req, res) => {
+	try {
+		const user = await userData.find({}).toArray();
+		const eersteKat = await kattenData.findOne({status: "new"}); // Zoekt de eerst volgende kat met status "new"
+	
+		console.log(user);
+		if(user.likedBy.includes(eersteKat._id)) {
+			kattenData.updateOne({_id: eersteKat_id}, {$set: {status: "liked"}})
+			res.redirect('/matched')
+		  } else {
+			kattenData.updateOne({_id: eersteKat_id}, {$set: {status: "liked"}})
+			res.redirect("/"); // De user wordt doorgestuurd naar home (refresh want de user is al op home)
+		}
+	} catch (error) {
+		console.error(error);
+	}	
 });
 
 app.post("/disliked", (req, res) => {
