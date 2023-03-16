2637518914985c637aca0dbb323e8d269b470bfa
enisokic <enis-okic@hotmail>
2023-03-16 13:50:49 +0100

    likedby werkt niet

--- server.js
+++ server.js
@@ -28,39 +28,33 @@ client.connect((err) => {
 	}
 });
 
-
-let db
-let kattenData
-let userData
+let db;
+let kattenData;
+let userData;
 
 async function run() {
 	try {
-	  await client.connect();
-	  console.log("hi");
-	  // database and collection code goes here    
-	  db = client.db("catmatch-data");
-	  kattenData = db.collection("katten");
-	  userData = db.collection("user");
-
-
+		await client.connect();
+		console.log("hi");
+		// database and collection code goes here
+		db = client.db("catmatch-data");
+		kattenData = db.collection("katten");
+		userData = db.collection("user");
 	} catch (error) {
-	  console.log(error);
+		console.log(error);
 	}
-  }
-  run();
-
+}
+run();
 
 // const katten = require("./data/katten.json"); // Json bestand met data geimporteerd die nu de naam katten heeft zodat ik deze data kan gebruiken in mijn code
 // const user = require("./data/user");
 
- app.get("/", async (req, res) => {
+app.get("/", async (req, res) => {
 	// Home pagina routing
 	try {
 		const eersteKat = await kattenData.findOne({ status: "new" }); // Zoekt de eerst volgende kat met status "new"
-		
-		res.render("verkennen.ejs", { eersteKat }); // Toont de pagina verkennen met de juist data
 
-		console.log(katten);
+		res.render("verkennen.ejs", { eersteKat }); // Toont de pagina verkennen met de juist data
 	} catch (error) {
 		console.error(error);
 	}
@@ -68,34 +62,89 @@ async function run() {
 
 app.get("/match", async (req, res) => {
 	try {
+		const katten = await kattenData.find({}).toArray();
+
+		console.log(katten);
 		res.render("match.ejs", { katten }); // Match pagina met als route /match
 	} catch (error) {
 		console.error(error);
 	}
 });
 
+// app.post("/liked", async (req, res) => {
+// 	try {
+// 		const eersteKat = await kattenData.findOne({ status: "new" }); // Zoekt de eerst volgende kat met status "new"
+
+// 		await kattenData.updateOne(
+// 			{ _id: eersteKat._id },
+// 			{ $set: { status: "liked" } }
+// 		);
+
+// 		res.redirect("/")
+
+// 		console.log(kattenData);
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// });
+
+// app.post("/liked", async (req, res) => {
+// 	try {
+// 		const user = await userData.find({}).toArray();
+// 		const eersteKat = await kattenData.findOne({ status: "new" }); // Zoekt de eerst volgende kat met status "new"
+
+// 		if (user.likedBy.includes(eersteKat._id))
+// 		 {
+// 			await kattenData.updateOne(
+// 				{ _id: eersteKat._id },
+// 				{ $set: { status: "liked" } }
+// 			);
+// 				res.redirect("/match");
+// 		} else {
+// 			await kattenData.updateOne(
+// 				{ _id: eersteKat._id },
+// 				{ $set: { status: "liked" } }
+// 			);
+
+// 			res.redirect("/");
+// 		}
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// });
+
 app.post("/liked", async (req, res) => {
 	try {
-		const user = await userData.find({}).toArray();
-		const eersteKat = await kattenData.findOne({status: "new"}); // Zoekt de eerst volgende kat met status "new"
-	
-		console.log(user);
-		if(user.likedBy.includes(eersteKat._id)) {
-			kattenData.updateOne({_id: eersteKat_id}, {$set: {status: "liked"}})
-			res.redirect('/matched')
-		  } else {
-			kattenData.updateOne({_id: eersteKat_id}, {$set: {status: "liked"}})
-			res.redirect("/"); // De user wordt doorgestuurd naar home (refresh want de user is al op home)
+		const user = await userData.findOne({});
+		// console.log(user);
+		const eersteKat = await kattenData.findOne({ status: "new" });
+
+		if (user.likedBy.includes(eersteKat._id)) {
+			await kattenData.updateOne(
+				{ _id: eersteKat._id },
+				{ $set: { status: "liked" } }
+			);
+			res.redirect("/match");
+		} else {
+			await kattenData.updateOne(
+				{ _id: eersteKat._id },
+				{ $set: { status: "liked" } }
+			);
+			res.redirect("/");
 		}
 	} catch (error) {
 		console.error(error);
-	}	
+	}
 });
 
-app.post("/disliked", (req, res) => {
-	const eersteKat = katten.find((kat) => kat.status === "new");
 
-	eersteKat.status = "disliked";
+app.post("/disliked", async (req, res) => {
+	const eersteKat = await kattenData.findOne({ status: "new" });
+
+	await kattenData.updateOne(
+		{ _id: eersteKat._id },
+		{ $set: { status: "disliked" } }
+	);
 	res.redirect("/");
 });
 
